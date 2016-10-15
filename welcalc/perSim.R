330e8ae739cb7d521a893adbc3510f235d8c2f4c
Brian Albert Monore <brianalbertmonroe@gmail.com>
2016-10-15 14:04:08 +0200

    Update welcalc stuff to include all RDU

--- welcalc/perSim.R
+++ welcalc/perSim.R
@@ -21,33 +21,87 @@ runInd <- function(d, K) {
 #	dbug <- 1
 #	print(paste("Here:", dbug)) ; dbug <- dbug + 1
 
+	# The format of the results of this function
+	results <- list(failed = 0, sim = NA, EUT = 0, pow = 0, invs = 0, prelec = 0)
+
+	optimizer <- "NR"
+
 	# What are the real EUT parameters
 	s.real <- c(d$r[1], d$mu[1])
 
 	# Be generous, start the optimizer at the real parameters
-	n <- tryCatch(ML.optim(pars = s.real, inst = d, pfunc = "EUT", optimizer = "BFGS", try = F, report = F), 
+	EUT <- tryCatch(ML.optim(pars = s.real, inst = d, pfunc = "EUT", optimizer = optimizer, try = F, report = F, clean = F), 
 					 error = function(x) return(NA))
 
 	rdu.real <- c(s.real[1], 1, s.real[2])
-	#o <- tryCatch(ML.optim(pars = rdu.real, inst = d, pfunc = "pow", optimizer = "BFGS", try = F, report = F), 
-	#				 error = function(x) return(NA))
-	o <- NA
+	pow <- tryCatch(ML.optim(pars = rdu.real, inst = d, pfunc = "pow", optimizer = optimizer, try = F, report = F, clean = F), 
+					 error = function(x) return(NA))
 
-	if (is.na(n[1]) & is.na(o[1])) {
-		return(NA)
-	} else if (is.na(n[1])) {
-		m <- o
-		mod <- "pow"
-	} else if (is.na(o[1])) {
-		m <- n
-		mod <- "EUT"
-	} else if (n$likelihood > o$likelihood) {
-		m <- n
-		mod <- "EUT"
-	} else if (o$likelihood > n$likelihood) {
-		m <- o
-		mod <- "pow"
+	invs <- tryCatch(ML.optim(pars = rdu.real, inst = d, pfunc = "invs", optimizer = optimizer, try = F, report = F, clean = F), 
+					 error = function(x) return(NA))
+
+	rdu.real <- c(s.real[1], 1, 1, s.real[2])
+	prelec <- tryCatch(ML.optim(pars = rdu.real, inst = d, pfunc = "prelec", optimizer = optimizer, try = F, report = F, clean = F), 
+					 error = function(x) return(NA))
+
+	POW.test <- F
+	INVS.test <- F
+	PRELEC.test <- F
+
+	# Test if the RDU models show a difference from EUT
+	if (!is.na(pow[1])) {
+		est <- pow$estimates
+		sd <- sqrt(diag(solve(-pow$hessian)))
+		POW.test <- abs((est[2] - 1)/sd[2]) > 2
+	} 
+
+	if (!is.na(invs[1])) {
+		est <- invs$estimates
+		sd <- sqrt(diag(solve(-invs$hessian)))
+		INVS.test <- abs((est[2] - 1)/sd[2]) > 2
+	} 
+
+	if (!is.na(prelec[1])) {
+		est <- prelec$estimates
+		sd <- sqrt(diag(solve(-prelec$hessian)))
+		PRELEC0.test <- abs((est[2] - 1)/sd[2]) > 2
+		PRELEC1.test <- abs((est[3] - 1)/sd[3]) > 2
+		PRELEC.test <- PRELEC0.test | PRELEC1.test
+	} 
+
+	# If the RDU model doesn't have converging SE, then the test is false
+	if (is.na(POW.test)) RDU.test <- F
+	if (is.na(INVS.test)) INVE.test <- F
+	if (is.na(PRELEC.test)) PRELEC.test <- F
+
+	# If EUT Exists, or if RDU is different from EUT, get the likelihoods
+	likes <- c()
+	likes[1] <- ifelse(!is.na(EUT[1]), EUT$likelihood, NA)
+	likes[2] <- ifelse(POW.test, pow$likelihood, NA)
+	likes[3] <- ifelse(INVS.test, invs$likelihood, NA)
+	likes[4] <- ifelse(PRELEC.test, prelec$likelihood, NA)
+
+	# If everything is NA return a bunch of NAs
+
+	if(all(is.na(likes))) {
+		results$failed <- 1
+		return(unlist(results))
 	}
+
+	# Get the EUT model and the RDU models that are different from EUT ranked
+	mods  <- c("EUT", "pow", "invs", "prelec")
+	mods  <- mods[!is.na(likes)]
+	likes <- likes[!is.na(likes)]
+ 
+ 	mods  <- mods[order(likes, decreasing = T)]
+ 	likes <- likes[order(likes, decreasing = T)]
+
+	# After the sorting above, the first element of mods contains the winning model
+	# This is either EUT or the RDU model that is different from EUT with the highest likelihood
+	mod <- mods[1]
+
+	# Use get to get the object of the same name
+	m <- get(mod)
 
 	# Will need to transform the real parameters into what the likelihood function would see
 	r.par <- s.real
@@ -62,16 +116,8 @@ runInd <- function(d, K) {
 	bindw <- tryCatch(welCalc(inst = d, pars = m$estimates, hessian = m$hessian, pfunc = mod, boot = T),
 										error = function(x) return(NA))
 
-	# If we were able to bootstrap, lets bootstrap
+	# If we were able to bootstrap, use the bootstrap
 	if (!is.na(bindw[1])) indw <- bindw
-
-#	# Get the transformed populaiton level parameters
-#	transforms.eut <- c(0,1,1,1,2)
-#	sim.eut <- it(sim.eut, transforms.eut)
-#
-#	# The welfare using the "real" population parameters
-#	welCalc.sim(inst = d, pars = sim.eut, pfunc = "EUT", boot = F)
-
 
 	# Solve the hessian to get the covariance matrix of the estimators
 	# Point Estimates
@@ -80,53 +126,29 @@ runInd <- function(d, K) {
 	# Boot Estimates
 	#welCalc.sim(inst = d, pars = K$estimates, hessian = K$hessian, pfunc = "EUT", boot = T)
 
-	if (is.na(indw[1]) & is.na(simw[1])) {
-		return(NA)
-	} else if (is.na(indw[1])) {
-		return(1)
-	} else if (is.na(simw[1])) {
-		return(0)
-	}
-
 	idiff <- abs(rwel - indw)
 	sdiff <- abs(rwel - simw)
 
-	if (idiff[1] > sdiff[1]) {
-		print("Simulated is closer to Real")
-		return(1)
+	if (is.na(idiff[1] > sdiff[1])) {
+		results$sim <- NA
+	} else if (idiff[1] > sdiff[1]) {
+		print(paste("Simulated is closer to Real:", mod))
+		results$sim <- 0
 	} else {
-		print("Individual is closer to Real")
-		return(0)
+		print(paste("Individual is closer to Real:", mod))
+		results$sim <- 1
 	}
 
-}
+	if (mod == "EUT")
+		results$EUT <- 1 
+	else if (mod == "pow")
+		results$pow <- 1 
+	else if (mod == "invs")
+		results$invs <- 1 
+	else if (mod == "prelec")
+		results$prelec <- 1 
 
-runSimEUT <- function(sim.eut) {
-
-	N <- 200
-	H <- 350
-	iter <- 100
-	control <- list()
-
-	names(sim.eut) <- c("rm", "rs", "um", "us", "ru")
-
-	D <- genChoice(sim.eut, N, inst="HNG", pfunc = "EUT")
-
-	K <- tryCatch(MSL.optim(pars = sim.eut, inst = D, pfunc = "EUT",  HH = H, optimizer = "BFGS", iterations = iter, try = F, control = control),
-				  error = function(x) return(NA))
-
-	if (is.na(K[1])) return(NULL)
-	#save(D, K, file="optimres.Rda")
-	#load("optimres.Rda")
-
-	perSID <- split(x=D, f=D$ID)
-
-	sim_ind <- lapply(perSID, runInd, K = K)
-
-	avg <- mean(unlist(sim_ind), na.rm = T)
-
-	print(avg)
-
-	return(avg)
+	return(unlist(results))
 
 }
+
--- welcalc/welcalc.R
+++ welcalc/welcalc.R
@@ -4,21 +4,95 @@ c.library("MSL","cgen","welfare","halton")
 c.source("perSim.R")
 
 # Set runs to be a multiple of the number of cores available
-NN <- c.cores()
+NN <- c.cores() - 1
 #NN <- 1
 
-rm <- hunif(NN, min = -0.50, max = 1.50)
-rs <- hunif(NN, min =  0.05, max = 1.50)
-um <- hunif(NN, min =  0.08, max = 0.80)
-us <- hunif(NN, min =  0.05, max = 0.50)
-ru <- hunif(NN, min = -0.67, max = 0.67)
+#rm <- hunif(NN, min = -0.50, max = 1.50)
+#rs <- hunif(NN, min =  0.05, max = 1.50)
+#um <- hunif(NN, min =  0.08, max = 0.80)
+#us <- hunif(NN, min =  0.05, max = 0.50)
+#ru <- hunif(NN, min = -0.67, max = 0.67)
+#
+#SIM <- data.frame(rbind(rm, rs, um, us, ru))
+#SIM
+#
+#
+#c.lapply(1:NN, function(i){
+#
+#	sim.eut <- SIM[,i]
+#	N <- 200
+#	H <- 150
+#	iter <- 50
+#	control <- list()
+#	D <- genChoice(sim.eut, N, inst="HNG", pfunc = "EUT")
+#	K <- MSL.optim(pars = sim.eut, inst = D, pfunc = "EUT",  optimizer = "BFGS", try = F)
+#	save(K, D, file=paste0("MSL",i,".Rda"))
+#
+#})
+#
+#stop()
 
-SIM <- data.frame(rbind(rm, rs, um, us, ru))
-SIM
+#runSimEUT <- function(sim.eut) {
+runSimEUT <- function(i) {
 
-res <- c.lapply(SIM, runSimEUT)
+	load(file=paste0("MSL",i,".Rda"))
+	sim.eut <- c(rm = mean(D$r), rs = sd(D$r), um = mean(D$mu), us = sd(D$r), ru = cor(D$r, D$mu))
 
-print(warnings())
+#	N <- 200
+#	H <- 150
+#	iter <- 50
+#	control <- list()
+#
+#	names(sim.eut) <- c("rm", "rs", "um", "us", "ru")
+#
+#	D <- genChoice(sim.eut, N, inst="HNG", pfunc = "EUT")
+	# Clean up the D
+	D <- ML.clean(D)
+#
+#	K <- tryCatch(MSL.optim(pars = sim.eut, inst = D, pfunc = "EUT",  HH = H, optimizer = "BFGS", iterations = iter, try = F, control = control),
+#				  error = function(x) return(NA))
+
+
+
+	if (is.na(K[1])) return(NULL)
+	#save(D, K, file="optimres.Rda")
+	#load("optimres.Rda")
+
+	perSID <- split(x=D, f=D$ID)
+
+	# Run the main estimation welfare calculation stuff
+	wel_res <- lapply(perSID, runInd, K = K)
+
+	res <- list()
+
+	# Grab the bits of the output and aggregate it
+	ind_fail <- lapply(wel_res, function(x) x[1])
+	res$fails <- mean(unlist(ind_fail), na.rm = T)
+	
+	ind_sim <- lapply(wel_res, function(x) x[2])
+	res$sim_avg <- mean(unlist(ind_sim), na.rm = T)
+
+	eut_win <- lapply(wel_res, function(x) x[3])
+	res$eut_avg <- mean(unlist(eut_win), na.rm = T)
+
+	pow_win <- lapply(wel_res, function(x) x[4])
+	res$pow_avg <- mean(unlist(pow_win), na.rm = T)
+
+	invs_win <- lapply(wel_res, function(x) x[5])
+	res$invs_avg <- mean(unlist(invs_win), na.rm = T)
+
+	prelec_win <- lapply(wel_res, function(x) x[6])
+	res$prelec_avg <- mean(unlist(prelec_win), na.rm = T)
+
+	print(unlist(res))
+
+	return(res)
+
+}
+
+res <- lapply(1:NN, runSimEUT)
+
+
 
 res
 
